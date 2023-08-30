91c3c49f822fb42e2eabd3c308859e09473df42f
Hossam Othman <rowheadrex@gmail.com>
2023-08-30 11:55:00 +0200

    Win Conditions + Game Over

--- app.js
+++ app.js
@@ -1,71 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    const dino = document.querySelector('.dino');
-    const grid = document.querySelector('.grid');
-    let isJumping = false;
-    let gravity = 0.9;
-    function control(e){
-        if(e.keyCode === 32 ) {
-            if (!isJumping) {
-                isJumping = true;
-                jump()
-            }
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  const dino = document.querySelector(".dino");
+  const grid = document.querySelector(".grid");
+  const alert = document.getElementById("alert");
+  let isJumping = false;
+  let gravity = 0.9;
+  let isGameover = false;
+  function control(e) {
+    if (e.keyCode === 32) {
+      if (!isJumping) {
+        isJumping = true;
+        jump();
+      }
     }
-    document.addEventListener('keydown', control)
+  }
+  document.addEventListener("keydown", control);
 
-    let position = 0;
-    function jump() {
+  let position = 0;
+  function jump() {
     let count = 0;
-       let timerId = setInterval(function() {
+    let timerId = setInterval(function () {
+      // move down
+      if (count == 15) {
+        clearInterval(timerId);
 
-        // move down
-        if (count == 15) {
-            clearInterval(timerId);
-
-            console.log('down');
-            let downTimerId = setInterval(function() {
-                if (count == 0) {
-                    clearInterval(downTimerId);
-                    isJumping = false;
-                }
-                position -= 13;
-                count--;
-                position *= (gravity + 0.1);
-                dino.style.bottom = position + 'px';
-            }, 20);
-        }
-
-        // move up
-        console.log('up');
-        count++
-        position += 30;
-        position *= gravity;
-        dino.style.bottom = position + 'px';
-        console.log(dino.style.bottom);
-       }, 20)
-    };
-
-    function generateObstacles() {
-        let randomTime = Math.random() * 4000;
-        let obstaclePosition = 1000;
-        const obstacle = document.createElement('div');
-        obstacle.classList.add('obstacle');
-        grid.appendChild(obstacle);
-        obstacle.style.left = obstaclePosition + 'px';
-
-        let timerId = setInterval(function() {
-            if (obstaclePosition === 0) {
-                clearInterval(timerId)
-                alert('Game Over')
-            }
-
-            obstaclePosition -= 10;
-            obstacle.style.left = obstaclePosition + 'px';
+        console.log("down");
+        let downTimerId = setInterval(function () {
+          if (count == 0) {
+            clearInterval(downTimerId);
+            isJumping = false;
+          }
+          position -= 13;
+          count--;
+          position *= gravity + 0.1;
+          dino.style.bottom = position + "px";
         }, 20);
-        setTimeout(generateObstacles, randomTime)
-    };
-    generateObstacles();
+      }
 
- 
-})
+      // move up
+      console.log("up");
+      count++;
+      position += 30;
+      position *= gravity;
+      dino.style.bottom = position + "px";
+      console.log(dino.style.bottom);
+    }, 20);
+  }
+
+  function generateObstacles() {
+    let randomTime = Math.random() * 4500;
+    let obstaclePosition = 1000;
+    const obstacle = document.createElement("div");
+    if (!isGameover) obstacle.classList.add("obstacle");
+    grid.appendChild(obstacle);
+    obstacle.style.left = obstaclePosition + "px";
+
+    let timerId = setInterval(function () {
+      if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+        clearInterval(timerId);
+        alert.innerHTML = "Game Over!";
+        isGameover = true;
+      }
+
+      obstaclePosition -= 10;
+      obstacle.style.left = obstaclePosition + "px";
+    }, 20);
+    if (!isGameover) setTimeout(generateObstacles, randomTime);
+  }
+  generateObstacles();
+});
